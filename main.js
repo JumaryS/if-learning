abc32a3da49b74a4ce3df11595ee2e8a3aa0c552
JumarySanchez <sanchezjumary@gmail.com>
2020-06-16 10:33:57 -0400

    stuck on the last 2

--- main.js
+++ main.js
@@ -1,7 +1,48 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
-
+function isItLong(string){
+  if (string.length >20){
+    return "That's a long string!"
+  }
+  }
+  
+  function isItMedium(string2){
+    if (string2.length >=10 && string2.length <=20 )
+  {
+    return "That's a regular sized string!"
+  } else{
+    return 
+  }
+  }
+  
+  function isItShort (string3){
+    if (string3.length <10){
+      return "That's a small string!"
+    } else{
+      return 'That\'s not a small string!'
+    }
+  }
+  
+  function howLongIsMyString(string4){
+    if(string4.length >20){
+      return "That's a long string!"}
+    else if (string4.length>= 10 && string4.length<=20)
+    { return "That's a regular sized string!"}
+      else if (string4.length <10)
+    {return "That's a small string!"}
+    
+  }
+  function instructorHeight(name){
+  if (name ==='Colin'){
+      return 62
+  }else if(name ==='Mesuara'){
+  return 67
+  }
+  else{
+      return 69
+    }
+    }
 
 
 
